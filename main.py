3ec76ed5e0a8f269a85373b63b1d2842c252af51
HLC2757808353 <2757808353@qq.com>
2026-02-23 13:53:26 +0800

    chore: 更新插件版本并优化导入语句
    将插件版本从v0.0.1升级至v0.0.3
    移除main.py中冗余的Tool导入，改为从astrbot.api.tool导入

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
-from astrbot.api.star import Context, Star, register, Tool
+from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from astrbot.api.tool import Tool
 import os
 import threading
 
